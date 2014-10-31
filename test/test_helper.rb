36f2ce26ac4c4cea71a16e92f4081bc881868d99
Masato Ikeda <masato.ikeda@gmail.com>
2014-10-31 21:53:25 +0900

    Setup testing tools
    Run tests by `rake` command.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
+$LOAD_PATH.unshift(File.expand_path("..", __FILE__))
+
+require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new]
+
+require "simple_xlsx"
