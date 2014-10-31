56c7f770b44d8bccdf96e14d4b3b016bf1a8ed98
Masato Ikeda <masato.ikeda@gmail.com>
2014-11-01 03:53:02 +0900

    Treat blank cells as nil

--- lib/simple_xlsx.rb
+++ lib/simple_xlsx.rb
@@ -1,13 +1,14 @@
 require "simple_xlsx/version"
 require "simple_xlsx/sheet"
 require "active_support/core_ext/object/try"
+require "active_support/core_ext/object/blank"
 require "rubyXL"
 
 module SimpleXLSX
   def self.read(path)
     book = RubyXL::Parser.parse(path)
     book[0].sheet_data.rows.map do |row|
-      row.cells.map { |cell| cell.try(:value) }
+      row.cells.map { |cell| cell.try(:value).presence }
     end
   end
 
--- lib/simple_xlsx/sheet.rb
+++ lib/simple_xlsx/sheet.rb
@@ -13,7 +13,7 @@ module SimpleXLSX
     def <<(cells)
       row = rubyxl_worksheet.sheet_data.rows.count
       cells.each_with_index do |value, column|
-        rubyxl_worksheet.add_cell(row, column, value)
+        rubyxl_worksheet.add_cell(row, column, value.presence)
       end
       cells
     end
