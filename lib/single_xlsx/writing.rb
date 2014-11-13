89fa7fa2e216967d93461d202dbde60e6e9e8ee0
Masato Ikeda <masato.ikeda@gmail.com>
2014-11-14 03:19:08 +0900

    Add SingleXLSX.open method to write XLSX file

--- lib/single_xlsx/writing.rb
+++ lib/single_xlsx/writing.rb
@@ -2,14 +2,24 @@ require "single_xlsx/sheet"
 
 module SingleXLSX
   module Writing
-    def generate
-      raise ArgumentError, "no block given" unless block_given?
-
-      book = RubyXL::Workbook.new
-      sheet = Sheet.new(book[0])
-      yield sheet
-      book.stream.read
+    def generate(&block)
+      create_book(&block).stream.read
     end
+
+    def open(path, &block)
+      create_book(&block).write(path)
+    end
+
+    private
+
+      def create_book(&block)
+        raise ArgumentError, "no block given" if block.nil?
+
+        book = RubyXL::Workbook.new
+        sheet = Sheet.new(book[0])
+        block.call(sheet)
+        book
+      end
   end
 
   extend Writing
